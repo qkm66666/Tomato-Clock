c9d96a94f4391506870f85f160f2709d928037d4
qkm66666 <739862481@qq.com>
2025-04-24 15:34:02 +0800

    将插件配置更新为 Manifest V3 标准

--- background.js
+++ background.js
@@ -1,16 +1,3 @@
-chrome.app.runtime.onLaunched.addListener(launch);
-
-function launch() {
-  chrome.app.window.create('window.html', {
-       id: 'main',
-       bounds: {
-           width: 1024,
-           height: 720,
-           left: 100,
-           top: 100
-       },
-       minWidth: 960,
-       minHeight: 640,
-       alwaysOnTop: true
-   });
-}
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Tomato 扩展已安装');
+});
